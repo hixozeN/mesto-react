643b8bda4543f04e3ea9bd37c65324a6032ce177
Vladislav K <aljkeee@gmail.com>
2023-04-20 02:13:36 +0300

    feat: добавить кружок загрузки

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -7,8 +7,10 @@ const Main = (props) => {
   const [userDescription, setUserDescription] = useState("");
   const [userAvatar, setUserAvatar] = useState("");
   const [cards, setCards] = useState([]);
+  const [isLoading, setLoading] = useState(true);
 
   useEffect(() => {
+    setLoading(true);
     Promise.all([api.getUserInfo(), api.getInitialCards()])
       .then(([userData, cardList]) => {
         setUserName(userData.name);
@@ -16,61 +18,74 @@ const Main = (props) => {
         setUserAvatar(userData.avatar);
         setCards(cardList);
       })
-      .catch((err) => console.error(err));
+      .catch((err) => console.error(err))
+      .finally(() => setLoading(false));
   }, []);
 
   return (
     <main className="page">
-      <section className="head-profile">
-        <div className="head-profile__user-section">
-          <div className="head-profile__avatar-section">
-            <img
-              className="head-profile__avatar"
-              src={userAvatar}
-              alt="Аватар пользователя"
-            />
-            <button
-              className="head-profile__edit-avatar-button"
-              type="button"
-              aria-label="Редактировать аватар"
-              onClick={props.onEditAvatar}
-            ></button>
-          </div>
-          <div className="head-profile__userinfo">
-            <div className="head-profile__username-and-edit">
-              <h1 className="head-profile__username">{userName}</h1>
-              <button
-                className="head-profile__edit-button"
-                type="button"
-                aria-label="Редактировать имя и деятельность"
-                onClick={props.onEditProfile}
-              ></button>
-            </div>
-            <p className="head-profile__job">{userDescription}</p>
-          </div>
+      {isLoading ? (
+        <div className="page__loading">
+          <span className="page__loading-text">Идет загрузка</span>
+          <div className="page__loading-circle"></div>
         </div>
-        <button
-          className="head-profile__add-button"
-          type="button"
-          aria-label="Добавить фото в ленту"
-          onClick={props.onAddPlace}
-        ></button>
-      </section>
+      ) : (
+        <>
+          <section className="head-profile">
+            <div className="head-profile__user-section">
+              <div className="head-profile__avatar-section">
+                <img
+                  className="head-profile__avatar"
+                  src={userAvatar}
+                  alt="Аватар пользователя"
+                />
+                <button
+                  className="head-profile__edit-avatar-button"
+                  type="button"
+                  aria-label="Редактировать аватар"
+                  onClick={props.onEditAvatar}
+                ></button>
+              </div>
+              <div className="head-profile__userinfo">
+                <div className="head-profile__username-and-edit">
+                  <h1 className="head-profile__username">{userName}</h1>
+                  <button
+                    className="head-profile__edit-button"
+                    type="button"
+                    aria-label="Редактировать имя и деятельность"
+                    onClick={props.onEditProfile}
+                  ></button>
+                </div>
+                <p className="head-profile__job">{userDescription}</p>
+              </div>
+            </div>
+            <button
+              className="head-profile__add-button"
+              type="button"
+              aria-label="Добавить фото в ленту"
+              onClick={props.onAddPlace}
+            ></button>
+          </section>
 
-      <section className="photo-feed" aria-label="Лента с карточками фото-мест">
-        {cards.map((card) => {
-          return (
-            <Card
-              key={card._id}
-              cardData={card}
-              name={card.name}
-              link={card.link}
-              likes={card.likes}
-              onCardClick={props.onCardClick}
-            />
-          );
-        })}
-      </section>
+          <section
+            className="photo-feed"
+            aria-label="Лента с карточками фото-мест"
+          >
+            {cards.map((card) => {
+              return (
+                <Card
+                  key={card._id}
+                  cardData={card}
+                  name={card.name}
+                  link={card.link}
+                  likes={card.likes}
+                  onCardClick={props.onCardClick}
+                />
+              );
+            })}
+          </section>
+        </>
+      )}
     </main>
   );
 };
